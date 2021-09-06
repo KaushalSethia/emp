707ec316372f27e0d83ff4f9a7fc5b9699d54b75
KaushalSethia <kksroxx95@gmail.com>
2021-09-06 11:07:00 +0530

    hibernate sessionFactory

--- src/main/java/org/example/dao/EmployeeDaoImpl.java
+++ src/main/java/org/example/dao/EmployeeDaoImpl.java
@@ -7,6 +7,7 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,32 +16,27 @@ import java.util.List;
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao{
 
-   /* @Autowired
-    private SessionFactory sessionFactory;*/
+
+    SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+
     @Inject
     private Session session;
 
-   /* @Autowired
-    public EmployeeDaoImpl(SessionFactory sessionFactory)
-    {
-        this.sessionFactory = sessionFactory;
-    }*/
-
     @Override
     public void save(Employee e) {
-        //Session session = this.sessionFactory.openSession();
-        //Transaction tx = session.beginTransaction();
-        //session.save(e);
-        session.persist(e);
-        //tx.commit();
-        //session.close();
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(e);
+        //session.persist(e);
+        tx.commit();
+        session.close();
 
     }
 
 
     @Override
     public List<Employee> getEmployee() {
-        //Session session = this.sessionFactory.openSession();
+        Session session = this.sessionFactory.openSession();
 
         Criteria cr = session.createCriteria(Employee.class);
         List results = cr.list();
